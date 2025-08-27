ef8355a22e1f7dd22f5288e554f837ad41a75147
Mika Yli-Pentti <mika.ylipentti@gmail.com>
2025-08-27 17:47:49 +0300

    Fixes

--- inc/client/client.h
+++ inc/client/client.h
@@ -6,13 +6,15 @@
 /*   By: myli-pen <myli-pen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/25 14:06:42 by myli-pen          #+#    #+#             */
-/*   Updated: 2025/08/27 17:04:09 by myli-pen         ###   ########.fr       */
+/*   Updated: 2025/08/27 17:45:41 by myli-pen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLIENT_H
 # define CLIENT_H
 
+// System
+// -----------------------------------------------------------------------------
 # define _POSIX_C_SOURCE 200112L
 # define _GNU_SOURCE
 
@@ -21,9 +23,17 @@
 # include <signal.h>
 # include <sys/types.h>
 
+// Libft
+// -----------------------------------------------------------------------------
 # include "libft_utils.h"
 # include "libft_str.h"
 
+// Minitalk
+// -----------------------------------------------------------------------------
 # include "utils.h"
+
+# ifndef PID_MAX
+#  define PID_MAX 4194304
+# endif
 
 #endif
--- inc/server/server.h
+++ inc/server/server.h
@@ -6,13 +6,15 @@
 /*   By: myli-pen <myli-pen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/25 14:06:53 by myli-pen          #+#    #+#             */
-/*   Updated: 2025/08/27 17:04:21 by myli-pen         ###   ########.fr       */
+/*   Updated: 2025/08/27 17:45:30 by myli-pen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SERVER_H
 # define SERVER_H
 
+// System
+// -----------------------------------------------------------------------------
 # define _POSIX_C_SOURCE 200112L
 # define _GNU_SOURCE
 
@@ -21,8 +23,12 @@
 # include <signal.h>
 # include <sys/types.h>
 
+// Libft
+// -----------------------------------------------------------------------------
 # include "libft_io.h"
 
+// Minitalk
+// -----------------------------------------------------------------------------
 # include "utils.h"
 
 typedef struct s_message
